a6a22972053216b466e7f3d98f4459357fc99865
root <root@DESKTOP-N8RER9V.localdomain>
2022-11-22 09:10:22 +0000

    remove unused imports

--- app/Models/CityWeatherResult.php
+++ app/Models/CityWeatherResult.php
@@ -4,15 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class CityWeatherResult extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
-    public function city(): \Illuminate\Database\Eloquent\Relations\HasOne
+    public function city(): HasOne
     {
         return $this->hasOne(City::class, 'id', 'city_id');
     }
